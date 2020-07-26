032e93006d1053b2aa52276cd3e96cc95d86dd55
angelicphan <phan4@pdx.edu>
2020-07-26 16:12:20 -0700

    change to build

--- server/server.js
+++ server/server.js
@@ -2,10 +2,15 @@ const path = require("path");
 const express = require("express");
 const app = express();
 const port = process.abort.env.PORT || 3000;
-const buildPath = path.join(__dirname, "..", "build");
+const buildPath = path.join(__dirname, "build");
 
 // Tell Express which files to serve
 app.use(express.static(buildPath));
+
+// Tell it to use index.html
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 app.listen(port, () => {
   console.log("Port: ", port);
