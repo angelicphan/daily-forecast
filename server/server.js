1309bcde0a60096e8c23dd16e9f5ba38267a9040
angelicphan <phan4@pdx.edu>
2020-07-26 15:49:43 -0700

    setting up to push to Heroku

--- server/server.js
+++ server/server.js
@@ -2,10 +2,10 @@ const path = require("path");
 const express = require("express");
 const app = express();
 const port = process.abort.env.PORT || 3000;
-const publicPath = path.join(__dirname, "..", "public");
+const buildPath = path.join(__dirname, "..", "build");
 
 // Tell Express which files to serve
-app.use(express.static(publicPath));
+app.use(express.static(buildPath));
 
 app.listen(port, () => {
   console.log("Port: ", port);
